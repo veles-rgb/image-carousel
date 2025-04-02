5ad52ff4315f41871bdbf252bcd2a8c185f62a3c
MacMini <181009546+veles-rgb@users.noreply.github.com>
2025-04-02 16:07:49 -0400

    Changes

---
+++ README.md
@@ -0,0 +1,11 @@
+# Image Carousel
+
+## Sources
+
+- Blurry Image of woman by [Jean-Daniel Francoeur](https://www.pexels.com/photo/dynamic-motion-blur-portrait-in-studio-setting-30936133/)
+
+- Photo of a bird by [John Hanson](https://www.pexels.com/photo/female-mallard-duck-standing-on-frozen-lake-31328987/)
+
+- Photo of a road by [DiePhotoPotato](https://www.pexels.com/photo/asphalt-road-in-countryside-20349744/)
+
+- Photo of leafs by [Bruno Cervera](https://www.pexels.com/photo/a-green-leaves-on-white-surface-13567728/)
--- src/js/index.js
+++ src/js/index.js
@@ -1,2 +1,53 @@
 // index.js
 import "../styles/styles.css";
+import "../styles/reset.css";
+
+const imageSlide = document.querySelector(".image-slide");
+const images = imageSlide.querySelectorAll("img");
+const leftArrow = document.querySelector(".left-arrow");
+const rightArrow = document.querySelector(".right-arrow");
+let currentIndex = 0;
+
+images[0].style.display = "block";
+
+function createCircles() {
+    images.forEach(image => {
+        const newCircle = document.createElement("div");
+
+    });
+}
+
+leftArrow.addEventListener("click", () => {
+    if (currentIndex === 0) {
+        currentIndex = images.length - 1;
+        showImage(currentIndex);
+    } else {
+        currentIndex--;
+        showImage(currentIndex);
+    }
+    hideImages();
+});
+
+rightArrow.addEventListener("click", () => {
+    if (currentIndex === images.length - 1) {
+        currentIndex = 0;
+        showImage(currentIndex);
+    } else {
+        currentIndex++;
+        showImage(currentIndex);
+    }
+    hideImages();
+});
+
+function showImage(index) {
+    images[index].style.display = "block";
+}
+
+function hideImages() {
+    images.forEach((image, index) => {
+        if (currentIndex !== index) {
+            image.style.display = "none";
+        }
+    });
+}
+
